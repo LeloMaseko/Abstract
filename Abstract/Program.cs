b6e03959a9aab124f0be212150ec01062ac89502
A253560 <A253560@LSBSA-CCTCSV3>
2023-12-05 18:32:24 +0200

    Add project files.

---
+++ Abstract/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Corsa car2 = new Corsa();
+            Console.WriteLine(car2.wheels);
+            Console.ReadLine();
+        }
+    }
+    abstract class Vehicle
+    {
+        public int wheels = 4;
+    }
+    class Corsa : Vehicle
+    {
+        public int sparewheels = 1;
+    }
+}
